0b2379641e80db7557a0decf642324044f511988
oocococo <oocococo@localhost.localdomain>
2019-03-28 12:30:03 +0800

    bug fix

--- main.go
+++ main.go
@@ -13,8 +13,6 @@ import (
 func main() {
 	var config Config
 	var wg sync.WaitGroup
-	var emp []Group
-	var empproxy []Proxy
 
 	rawconfig, err := ioutil.ReadFile("config.yml")
 	if err != nil {
@@ -24,8 +22,8 @@ func main() {
 	if err != nil {
 		fmt.Println("配置格式错误", err)
 	}
-	config.ProxyGroup = emp
-	config.Proxy = empproxy
+	config.ProxyGroup = nil
+	config.Proxy = nil
 	var proxiesname []string
 	//read surge sub and transform into proxy
 	source := ReadSource()
@@ -37,10 +35,8 @@ func main() {
 				defer wg.Done()
 				if rawconfig != nil {
 					newproxy := FormatProxy(rawproxy)
-					if newproxy.Name != "" {
-						config.Proxy = append(config.Proxy, newproxy)
-						proxiesname = append(proxiesname, newproxy.Name)
-					}
+					config.Proxy = append(config.Proxy, newproxy)
+					proxiesname = append(proxiesname, newproxy.Name)
 
 				}
 				//fmt.Println("成功识别第", i, "个节点")
@@ -50,52 +46,52 @@ func main() {
 		fmt.Println("成功读取所有节点")
 	}
 	//generate group
-	wg.Add(len(source.Grouplist))
 	for g := 0; g < len(source.Grouplist); g++ {
-		go func(wantedlist GroupList) {
-			defer wg.Done()
-			var autogroup Group
-			var afterdemand []string
-			needs := strings.Split(wantedlist.Demand, ",")
-			fmt.Println("允许关键词", needs)
-			trash := strings.Split(wantedlist.Abandon, ",")
-			fmt.Println("不允许关键词", trash)
-			autogroup.Name = wantedlist.Name
-			autogroup.Interval = wantedlist.Interval
-			autogroup.Type = wantedlist.Type
-			autogroup.URL = wantedlist.URL
-			for p := 0; p < len(proxiesname); p++ {
-				if needs == nil {
-					afterdemand = proxiesname
-					break
-				} else {
-					for n := 0; n < len(needs); n++ {
-						if need, _ := regexp.MatchString(needs[n], proxiesname[p]); need {
-							afterdemand = append(afterdemand, proxiesname[p])
-						}
+		var black int
+		var white int
+		var autogroup Group
+		var afterdemand []string
+		needs := strings.Split(source.Grouplist[g].Demand, ",")
+		fmt.Println("允许关键词", needs)
+		trash := strings.Split(source.Grouplist[g].Abandon, ",")
+		fmt.Println("不允许关键词", trash)
+		autogroup.Name = source.Grouplist[g].Name
+		autogroup.Interval = source.Grouplist[g].Interval
+		autogroup.Type = source.Grouplist[g].Type
+		autogroup.URL = source.Grouplist[g].URL
+		for p := 0; p < len(proxiesname); p++ {
+			if needs == nil {
+				afterdemand = proxiesname
+				break
+			} else {
+				for n := 0; n < len(needs); n++ {
+					if need, _ := regexp.MatchString(needs[n], proxiesname[p]); need {
+						afterdemand = append(afterdemand, proxiesname[p])
+						white += 1
 					}
 				}
 			}
-			fmt.Println("成功得到白名单节点")
-			//fmt.Print(afterdemand)
-			for p := 0; p < len(afterdemand); p++ {
-				if trash[0] == "" {
-					autogroup.Proxies = afterdemand
-					break
-				} else {
-					for a := 0; a < len(trash); a++ {
-						//fmt.Println(trash[a], "!!!")
-						if neednt, _ := regexp.MatchString(trash[a], afterdemand[p]); !neednt {
-							autogroup.Proxies = append(autogroup.Proxies, afterdemand[p])
-						}
+		}
+		//fmt.Print(afterdemand)
+		for p := 0; p < len(afterdemand); p++ {
+			if trash[0] == "" {
+				autogroup.Proxies = afterdemand
+				black = white
+				break
+			} else {
+				for a := 0; a < len(trash); a++ {
+					//fmt.Println(trash[a], "!!!")
+					if neednt, _ := regexp.MatchString(trash[a], afterdemand[p]); !neednt {
+						autogroup.Proxies = append(autogroup.Proxies, afterdemand[p])
+						black += 1
 					}
 				}
 			}
-			fmt.Print("成功从白名单中删除不需要的节点")
-			config.ProxyGroup = append(config.ProxyGroup, autogroup)
-		}(source.Grouplist[g])
+		}
+		fmt.Println(autogroup.Name, "组更新了", black, "个节点")
+		config.ProxyGroup = append(config.ProxyGroup, autogroup)
 	}
-	wg.Wait()
+
 	outputconfig, err := yaml.Marshal(config)
 	clasherr := ioutil.WriteFile("config.yml", outputconfig, 0644)
 	Checkerr(clasherr)
